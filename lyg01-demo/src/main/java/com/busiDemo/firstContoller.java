3dc545851a69240858799e0cb27ea569aeb9bdda
jerweiwang <why602@163.com>
2019-01-20 11:01:31 +0800

    first branch change commit

--- lyg01-demo/src/main/java/com/busiDemo/firstContoller.java
+++ lyg01-demo/src/main/java/com/busiDemo/firstContoller.java
@@ -5,15 +5,19 @@ package com.busiDemo;/**
  * @create 2019-01-13 16:56
  **/
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * first
  * @author why
  * @create 2019-01-13 16:56
  **/
-
+@RestController
 public class firstContoller {
 
     public static void main(String[] args) {
         System.out.print("===first branch change");
     }
+
+
 }
---
+++ lyg01-demo/src/main/java/com/busiDemoService/firstService.java
@@ -0,0 +1,18 @@
+package com.busiDemoService;/**
+ * firstService
+ *
+ * @author why
+ * @create 2019-01-20 10:56
+ **/
+
+import org.springframework.stereotype.Service;
+
+/**
+ * firstService
+ * @author why
+ * @create 2019-01-20 10:56
+ **/
+@Service
+public class firstService {
+
+}
